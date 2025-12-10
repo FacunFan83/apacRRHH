0654a5e91639ec1b39b8d256aa06b4802283be8d
FacunFan83 <126334032+FacunFan83@users.noreply.github.com>
2025-12-10 19:24:51 -0300

    Middleware Not Found

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express"
+import path from "node:path"
+import { fileURLToPath } from "node:url";
 
 const app = express()
-app.get("/", (req, res) => {
-    res.send('Hola como estas')
-})
-const PORT = 3000
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+import notFound from "./src/middlewares/notFound.js"
+app.use(notFound)
+
+const PORT = 3000
 app.listen(PORT)
